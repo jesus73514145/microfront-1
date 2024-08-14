fe29d1c9f888ee7febda76f11a53594d7a3982a9
Jesus Soria <jesus_soria@usmp.pe>
2024-08-14 06:59:51 -0500

    Archivos iniciales
    Co-authored-by: AlbertoSoria1998 <albertosoriasoria74@gmail.com>

---
+++ src/components/about/About.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../navbar/Navbar"
+
+export default function About(){
+    return(
+        <>
+        <Navbar/>
+        <div>
+            <h1>About</h1>
+            <p>Esta es la página sobre</p>
+        </div>
+        </>
+    )
+}
